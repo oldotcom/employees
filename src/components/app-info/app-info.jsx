5266f7ac6ce611036864c1b6e1123a871b65f103
Oleksa <oleksa.antsupov@gmail.com>
2022-09-27 15:46:58 +0300

    add app-info component

---
+++ src/components/app-info/app-info.jsx
@@ -0,0 +1,13 @@
+import './app-info.css';
+
+const AppInfo = () => {
+    return (
+        <div className="app-info">
+            <h1>Accounting of employees in the company</h1>
+            <h2>Total of employees: </h2>
+            <h2>Receive a bonus: </h2>
+        </div>
+    );
+}
+
+export default AppInfo;
