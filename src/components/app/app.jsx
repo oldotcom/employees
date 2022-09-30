c12cebfa5fe0784fa906757cde7412cd2f334a65
Oleksa <oleksa.antsupov@gmail.com>
2022-09-30 16:27:00 +0300

    add new props in the employees-list-item component

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -8,9 +8,9 @@ import './app.css';
 function App() {
 
     const data = [
-        {name: 'John C.', salary: 800},
-        {name: 'Alex M.', salary: 3000},
-        {name: 'Carl W.', salary: 6000}
+        {name: 'John C.', salary: 800, increase: true},
+        {name: 'Alex M.', salary: 3000, increase: false},
+        {name: 'Carl W.', salary: 6000, increase: true}
     ];
 
     return (
