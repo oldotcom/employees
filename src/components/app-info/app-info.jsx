706c5104029c86f0eb844fe7fba6a9c5cba409d4
Oleksa <oleksa.antsupov@gmail.com>
2022-11-17 17:25:47 +0200

    add total of employees & reseive a bonus, edit onToggleRise

--- src/components/app-info/app-info.jsx
+++ src/components/app-info/app-info.jsx
@@ -1,11 +1,11 @@
 import './app-info.css';
 
-const AppInfo = () => {
+const AppInfo = ({employees, increased}) => {
     return (
         <div className="app-info">
             <h1>Accounting of employees in the company</h1>
-            <h2>Total of employees: </h2>
-            <h2>Receive a bonus: </h2>
+            <h2>Total of employees: {employees}</h2>
+            <h2>Receive a bonus: {increased}</h2>
         </div>
     );
 }
--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -18,6 +18,7 @@ class App extends Component {
                 {name: 'Carl W.', salary: 6000, increase: true, rise: false, id: 3}
             ]
         }
+
         this.maxId = 4;
     }
 
@@ -47,18 +48,6 @@ class App extends Component {
     }
 
     onToggleIncrease = (id) => {
-        // this.setState(({data}) => {
-        //     const index = data.findIndex(elem => elem.id === id);
-            
-        //     const old = data[index];
-        //     const newItem = {...old, increase: !old.increase};
-        //     const newArr = [...data.slice(0, index), newItem, ...data.slice(index + 1)];
-
-        //     return {
-        //         data: newArr
-        //     }
-        // })
-
         this.setState(({data}) => ({
             data: data.map(item => {
                 if (item.id === id) {
@@ -70,13 +59,23 @@ class App extends Component {
     }
 
     onToggleRise = (id) => {
-        console.log(`Rise this ${id}`);
+        this.setState(({data}) => ({
+            data: data.map(item => {
+                if (item.id === id) {
+                    return {...item, rise: !item.rise}
+                }
+                return item;
+            })
+        }))
     }
 
     render() {
+        const employees = this.state.data.length;
+        const increased = this.state.data.filter(item => item.increase).length;
         return (
             <div className="app">
-                <AppInfo />
+                <AppInfo employees={employees}
+                         increased={increased} />
     
                 <div className="search-panel">
                     <SearchPanel />
